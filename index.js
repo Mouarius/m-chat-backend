0469d3be651828d50a51986b28a594f0099ca202
mouarius <mouarius@gmail.com>
2021-01-25 01:35:14 +0100

    First chat working version

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
+const http = require("http")
 const express = require("express")
-
-const app = express()
+const socketio = require("socket.io")
 const cors = require("cors")
 
 const PORT = 8000
 
+const generateID = () => Math.floor(Math.random() * 1000)
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+})
+
 app.use(cors())
+
+io.on("connection", (socket) => {
+  console.log("A user has connected")
+
+  socket.emit("connected", socket.id)
+
+  socket.on("send message", (message) => {
+    const id = generateID()
+    io.emit("message", { ...message, id })
+    console.log(message)
+  })
+
+  socket.on("disconnect", () => {
+    console.log("A user has disconnected")
+  })
+})
 
 app.get("/", (req, res) => {
   res.send("Hello World from server !")
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
